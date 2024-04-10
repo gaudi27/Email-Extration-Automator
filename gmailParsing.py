ab60cec858e59b316e0361dd711e1ebb5a1625ac
George Audi <144143470+gaudi27@users.noreply.github.com>
2024-04-10 17:36:58 -0400

    Update gmailParsing.py
    removing the excel paster and putting the function into a new file to make proccess easier, also added a dictionary for each new email

--- gmailParsing.py
+++ gmailParsing.py
@@ -9,8 +9,6 @@ import email
 import imaplib
 #getting username and password to be able to use said email
 import yaml
-#pasting gmail info into excel
-import xlsxwriter
 
 #opens the yaml file with username and password and uses them to log in to email
 with open("usernameAndPassword.yml") as f:
@@ -31,7 +29,7 @@ imapGmail.select('Inbox')
 #enter the email or contrants of the type of information you need 
 #to get from the emails
 key = "FROM"
-gmail = "exampleEmail@gmail.com"
+gmail = "gzaudi738@gmail.com"
 
 #gets the data from the inbox
 _, data = imapGmail.search(None, key, gmail)
@@ -52,6 +50,8 @@ for i in getIDs:
 '''I now have all the messages but with alot of unneeded data
 I want to extract only the text'''
 
+data = []
+
 for emls in emailBody[::-1]:
     for response in emls:
         if type(response) is tuple:
@@ -60,44 +60,13 @@ for emls in emailBody[::-1]:
             print ("subj:", my_eml['subject'])
             print ("from:", my_eml['from'])
             print ("body:")
+            #ADDING KEYS AND VALUES TO THE DICTIONARY "data" TO THEN PRINT IT TO EXCEL
+            dic = {}
+            dic["sender"] = my_eml['from']
+            dic["subject"] = my_eml['subject']
             for part in my_eml.walk():  
                 #print(part.get_content_type())
                 if part.get_content_type() == 'text/plain':
                     print (part.get_payload())
-            #ADDING KEYS AND VALUES TO THE DICTIONARY "data" TO THEN PRINT IT TO EXCEL
-            dic = {}
-
-            dic["sender"] = 
-            dic["email"] = my_eml['from']
-            dic["subject"] = my_eml['subject']
-            dic["body"] = "gakjhsfl;ahgdkjflas;augdhfkaj"
-
-
-
-
-'''PASTING INFO INTO EXCEL'''
-
-data = {
-        
-        }
-
-def infoPaster(data):
-    
-    workbook = xlsxwriter.Workbook("GmailsInfo.x1sx")
-    worksheet = workbook.add_worksheet("firstSheet")
-    
-    worksheet.write(0, 0, "#") 
-    worksheet.write(0, 1, "Sender") 
-    worksheet.write(0, 2, "Email") 
-    worksheet.write(0, 3, "Subject") 
-    worksheet.write(0, 4, "Body") 
-    
-    
-    for index, entry in enumerate(data):
-        worksheet.write(index+1, 0, str(index))
-        worksheet.write(index+1, 1, entry["Sender"])
-        worksheet.write(index+1, 2, entry["Email"]) 
-        worksheet.write(index+1, 3, entry["Subject"]) 
-        worksheet.write(index+1, 4, entry["Body"]) 
-
-    workbook.close()
+                    dic["body"] = part.get_payload()
+            data.append(dic)
