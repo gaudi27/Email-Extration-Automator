6d9ab578e22efc45008bb4129122ea90efb40a67
George Audi <144143470+gaudi27@users.noreply.github.com>
2024-04-10 18:24:32 -0400

    Update excelPaster.py
    condensed the code for efficiency and switched to openpyxl

--- excelPaster.py
+++ excelPaster.py
@@ -8,26 +8,16 @@ Date: April 9th 2024'''
 into an excel sheet'''
 
 
-import xlsxwriter
 
+import openpyxl
+from openpyxl import Workbook, load_workbook
 
 #creates excel and pasts emails in an orginized way
 
-def infoPaster(data):
-    
-    workbook = xlsxwriter.Workbook("GmailsInfo.x1sx")
-    worksheet = workbook.add_worksheet("firstSheet")
-    
-    worksheet.write(0, 0, "#") 
-    worksheet.write(0, 1, "Sender") 
-    worksheet.write(0, 3, "Subject") 
-    worksheet.write(0, 4, "Body") 
-    
-    
-    for index, entry in enumerate(data):
-        worksheet.write(index+1, 0, str(index))
-        worksheet.write(index+1, 1, entry["Sender"])
-        worksheet.write(index+1, 3, entry["Subject"]) 
-        worksheet.write(index+1, 4, entry["Body"]) 
 
-    workbook.close()
+
+def Paster(data):
+    wb = load_workbook('List of Emails to be used.xlsx')
+    ws = wb.active
+    ws.append(data)
+    wb.save('List of Emails to be used.xlsx')
