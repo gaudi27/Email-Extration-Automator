f1b24649693f9a767fcd0f7c3976913aeafdbd9f
George Audi <144143470+gaudi27@users.noreply.github.com>
2024-04-14 15:40:45 -0400

    Update gmailParsing.py
    made the code fully functional and auto updating

--- gmailParsing.py
+++ gmailParsing.py
@@ -15,6 +15,11 @@ import excelPaster
 #auto update the spreadsheet when an email comes in
 count = 0
 x = 0
+#a list made to extract the whole emails
+emailBody = []
+INFO = []
+
+
 #opens the yaml file with username and password and uses them to log in to email
 with open("usernameAndPassword.yml") as f:
     text = f.read()
@@ -32,13 +37,14 @@ while True:
     imapGmail.login(username, password)
     #location of the emails you want parsed
     imapGmail.select('Inbox')
-
-
+    
+    
     
     #enter the email or contrants of the type of information you need 
     #to get from the emails
     key = "FROM"
     gmail = "gzaudi738@gmail.com"
+    
     
     #gets the data from the inbox
     _, data = imapGmail.search(None, key, gmail)
@@ -49,44 +55,40 @@ while True:
     #get the IDs of the emails that are applicable to what is needed
     getIDs = data[0].split()
     
-    #a list made to extract the whole emails
-    emailBody = []
+    
     #going through the list of emails and putting them into the emailBody list
+    
     
     
     for i in getIDs:
         typ, data = imapGmail.fetch(i, '(RFC822)')
         emailBody.append(data)
-        count+=1
     
-    count -= x
     
     '''I now have all the messages but with alot of unneeded data
     I want to extract only the text'''
     
     
+    for emls in emailBody[::-1]:
+        for response in emls:
+            if type(response) is tuple:
+                my_eml = email.message_from_bytes((response[1]))
+                print("_________________________________________")
+                print ("subj:", my_eml['subject'])
+                print ("from:", my_eml['from'])
+                print ("body:")
+                #ADDING KEYS AND VALUES TO THE DICTIONARY "data" TO THEN PRINT IT TO EXCEL
+                dic = {}
+                dic["sender"] = my_eml['from']
+                dic[""] = ""
+                dic["subject"] = my_eml['subject']
+                dic[""] = ""
+                for part in my_eml.walk():  
+                    #print(part.get_content_type())
+                    if part.get_content_type() == 'text/plain':
+                        print(part.get_payload())
+                        dic["body"] = part.get_payload()
+                if dic not in INFO:
+                    INFO.append(dic)
+                    excelPaster.Paster(dic)
     
-    if count > 0:
-        for emls in emailBody[::-1]:
-            count-=1
-            x+=1
-            for response in emls:
-                if type(response) is tuple:
-                    my_eml = email.message_from_bytes((response[1]))
-                    print("_________________________________________")
-                    print ("subj:", my_eml['subject'])
-                    print ("from:", my_eml['from'])
-                    print ("body:")
-                    #ADDING KEYS AND VALUES TO THE DICTIONARY "data" TO THEN PRINT IT TO EXCEL
-                    data = []
-                    data.append(my_eml['from'])
-                    data.append("")
-                    data.append(my_eml['subject'])
-                    data.append("")
-                    for part in my_eml.walk():  
-                        #print(part.get_content_type())
-                        if part.get_content_type() == 'text/plain':
-                            print (part.get_payload())
-                            data.append(part.get_payload()) 
-                    excelPaster.Paster(data)
-
