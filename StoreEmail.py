88698e22a0cac6d4b329cf6557b8d3ff138cff4d
George Audi <144143470+gaudi27@users.noreply.github.com>
2024-05-13 20:00:02 -0400

    Update StoreEmail.py
    added a checker to see if new xlsx file is needed

--- StoreEmail.py
+++ StoreEmail.py
@@ -11,6 +11,33 @@ and stops repeats from printing in the excel sheet"""
 
 #use openpyxl to read the emails for repeats
 import openpyxl
+import os
+
+
+#checks to see if file exists, if not it creates a new file for the emails to be pasted into
+def excel_file_exists(file_path):
+    if os.path.exists(file_path):
+        try:
+            wb = openpyxl.load_workbook(filename=file_path)
+            wb.close()  # Close the workbook if it's successfully opened
+            return True
+        except:
+            return False
+    else:
+        return False
+
+
+#checks for if a new file needs creation
+def needNewFile():
+    file_path = "List of Emails to be used.xlsx"
+    if excel_file_exists(file_path) == False:
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        x = ["Sender", " ","Subject", "Body"]
+        ws.append(x)
+        ws.title = "List of Emails to be used"
+        wb.save(filename=file_path)
+
 
 def EmailStorage(dic):
     sheet = openpyxl.load_workbook("List of Emails to be used.xlsx")
@@ -20,7 +47,8 @@ def EmailStorage(dic):
     for i in range(1, maxRow):
         bodyCells = sheetData.cell(row = i, column = 4)
         bodyList.append(bodyCells.value)
+    #checks to see if email is already in the sheet so theres no repeats
     if dic["body"] in bodyList:
         return True
     return False
-    
+
